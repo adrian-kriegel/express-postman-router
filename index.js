fed181eacbb9f1a00f06ab417087be33c299bebe
Adrian Kriegel <info@unolog.in>
2019-12-11 15:15:42 +0100

    Added query parameters & cleanup

--- index.js
+++ index.js
@@ -93,90 +93,146 @@ function errRes(code, msg='', data={})
 }
 module.exports.errRes = errRes
 
-function checkParameters(req, desc, validator)
+function checkRequest(req, desc, validator)
 {
 	return new Promise((resolve, reject) =>
 	{
-		if(desc.params)
+		var body = checkParameters(req.body, desc.params, validator)
+
+		if(body) 
 		{
-			for(var name in desc.params)
+			reject(body)
+			return
+		}
+
+		var query = checkParameters(req.query, desc.query, validator)
+
+		if(query) 
+		{
+			reject(query)
+			return
+		}
+
+		var files = checkFiles(req, desc)
+
+		if(files) 
+		{
+			reject(files)
+			return
+		}
+
+		resolve()
+		
+	})
+}
+
+function checkParameters(body, params, validator)
+{
+	if(params)
+	{
+		for(var name in params)
+		{
+			const param = params[name]
+			var bodyparam = body[name]
+
+			if(!bodyparam)
 			{
-				const param = desc.params[name]
-				var bodyparam = req.body[name]
-
-				if(!bodyparam)
+				if(param.required) return result(null, 
 				{
-					if(param.required)
+					code: errcodes.BAD_REQUEST,
+					msg: 'Missing parameter: ' + name,
+					data: 
 					{
-						const e = result(null, 
-						{
-							code: errcodes.BAD_REQUEST,
-							msg: 'Missing parameter: ' + name 
-						})
+						param: name
+					} 
+				})
 
-						reject(e)
-
-						return
-
-					}else
-					{
-						continue
-					}
-				}
-
-				//parse the JSON if necessary
-				if(param.schema.type != 'string')
-				{
-					try
-					{
-						bodyparam = JSON.parse(bodyparam)
-
-					}catch(e)
-					{
-						reject(result(null, 
-						{
-							code: errcodes.BAD_REQUEST,
-							msg: 'Invalid JSON: ' + name,
-							data: 
-							{
-								param: name
-							} 
-						}))
-
-						return
-					}
-				}
-
-				//check if the schema matches
-				const valres = validator.validate(bodyparam, param.schema)
-				
-				if(valres.errors.length != 0)
-				{
-					const e = result(null, 
-					{
-						code: errcodes.BAD_REQUEST,
-						msg: 'Schema error: ' + name,
-						data: 
-						{
-							validationError: valres.errors,
-							parameter: name
-						} 
-					})
-
-					reject(e)
-				}
-
-				req.body[name] = param.schema.process ? param.schema.process(bodyparam) : bodyparam
+				return
 			}
 
-			resolve()
+			//parse the JSON if necessary
+			if(param.schema.type != 'string')
+			{
+				try
+				{
+					bodyparam = JSON.parse(bodyparam)
 
-		}else
-		{
-			//no parameters are required
-			resolve()
+				}catch(e)
+				{
+					return result(null, 
+					{
+						code: errcodes.BAD_REQUEST,
+						msg: 'Invalid JSON: ' + name,
+						data: 
+						{
+							param: name
+						} 
+					})
+				}
+			}
+
+			//check if the schema matches
+			const valres = validator.validate(bodyparam, param.schema)
+
+			if(valres.errors.length != 0)
+			{
+				console.log("awd")
+				return result(null, 
+				{
+					code: errcodes.BAD_REQUEST,
+					msg: 'Schema error: ' + name,
+					data: 
+					{
+						validationError: valres.errors,
+						parameter: name
+					} 
+				})
+			}
+
+			body[name] = param.schema.process ? param.schema.process(bodyparam) : bodyparam
 		}
-	})
+
+	}
+}
+
+function checkFiles(req, desc)
+{
+	if(desc.files)
+	{
+		if(!req.files) req.files = {}
+
+		for(var fname in desc.files)
+		{
+			const file = desc.files[fname]
+				
+			//check if the file is required but missing form the request
+			if(file.required && !(fname in req.files))
+			{
+				return errRes(errcodes.BAD_REQUEST,'Missing file(s): ' + fname, { file: fname })
+			}
+				
+			//if the file is included in the request
+			if((fname in req.files))
+			{
+				//if a single file is submitted, treat it as an array with one entry
+				if(!Array.isArray(req.files[fname]))
+				{
+					req.files[fname] = [req.files[fname]]
+				}
+
+				var reqFiles = req.files[fname]
+
+				//check the mime-type for every single file
+				for(var i in reqFiles)
+				{
+					if(file.mimetypes && !file.mimetypes.includes(reqFiles[i].mimetype) )
+					{
+						return errRes(errcodes.BAD_REQUEST,'Invalid mimetypes: ' + fname, { file: fname })	
+					}
+				}
+			}
+		}
+	}
 }
 
 //used to create default options
@@ -187,23 +243,25 @@ module.exports.options = function(name, args)
 
 function updateAllCollections()
 {
-	for(var collection_iud in postmanCollections)
+	for(var collection_uid in postmanCollections)
 	{
 		const routers = postmanCollections[collection_uid]
 
 		//TODO: wow this is ugly but it works since there has to be one element in there anyway
-		const apiKey = routers[0].postman.apiKey
+		const apikey = routers[0].postman.apikey
 
-		getPostmanCollection(apiKey, collection_iud).then((collection) =>
+		getPostmanCollection(apikey, collection_uid).then((collection) =>
 		{
 			for(var i in routers)
 			{
 				routers[i].updatePostmanCollection(collection)
 			}
+			updatePostmanCollection(apikey, collection_uid, collection).then(console.log).catch(console.error)
 
-			updatePostmanCollection(apiKey, collection_uid, collection)
-
-		}).catch(console.error)
+		}).catch((e) =>
+		{
+			console.error(e)
+		})
 	}
 }
 
@@ -220,7 +278,7 @@ cli.register('pr-ud', (name) =>
 	{
 		if(name in instances)
 		{
-			instances[name].updatePostman()
+			instances[name].updatePostman().then(console.log).catch(console.error)
 			return 'Started updating collection.'
 
 		}else
@@ -269,6 +327,8 @@ class PostmanRouter
 		this.mountpath = args.mountpath || '/'
 
 		this.host = args.host || process.env.HOSTNAME || ''
+
+		this.port = args.port || process.env.SERVER_PORT || 80
 
 		this.validator = new Validator()
 
@@ -337,19 +397,11 @@ class PostmanRouter
 		this.validator.addSchema(schema, id || schema.id)
 	}
 
-	/**
-		Adds a request to the API
-	*/
-	add(desc)
+	initParams(params)
 	{
-		if(typeof(desc.route) != 'string')
+		for(var p in params)
 		{
-			throw 'invalid route'
-		}
-
-		for(var p in desc.params)
-		{
-			const param = desc.params[p]
+			const param = params[p]
 
 			//apply inheritance
 			if(param.extends)
@@ -389,7 +441,39 @@ class PostmanRouter
 				}
 			}
 		}
+	}
 
+	/**
+		Adds a request to the API
+	*/
+	add(desc)
+	{
+		if(typeof(desc.route) != 'string')
+		{
+			throw 'invalid route'
+		}
+
+		if(!desc.enctype)
+		{
+			desc.enctype = this.enctype
+		}
+
+		desc.params = desc.params || {}
+		desc.files = desc.files || {}
+
+		this.initParams(desc.params)
+		this.initParams(desc.query)
+
+		//add a type attribute to files in order to display the information in the collection
+		//also add allowed mimetypes to desctiption
+		for(var fname in desc.files)
+		{
+			const file = desc.files[fname]
+
+			file.type = 'file'
+
+			file.description = (file.mimetypes || '["*"]') + ' ' + file.description
+		}
 
 		desc.method = desc.method || this.method
 
@@ -408,7 +492,7 @@ class PostmanRouter
 				return	
 			} 
 
-			checkParameters(req, desc, this.validator).then((e) =>
+			checkRequest(req, desc, this.validator).then((e) =>
 			{
 				next()
 
@@ -426,7 +510,7 @@ class PostmanRouter
 		for(var i in desc.callback)
 		{
 			const callback = desc.callback[i]
-
+			
 			switch(desc.method)
 			{
 				case 'POST':
@@ -453,12 +537,16 @@ class PostmanRouter
 
 			if(!route.hidden)
 			{
+				//original value from the collection
+				var oldRoute = null
+				//we need to index in order to replace it in the end
 				var oldRouteIndex = null
 
 				for(var j in collection.item)
 				{
 					if(collection.item[j].name == route.name)
 					{
+						oldRoute = collection.item[j]
 						oldRouteIndex = j
 					}
 				}
@@ -467,64 +555,36 @@ class PostmanRouter
 
 				var desc = route.description
 
-				if(route.params)
+				var oldFormdata = null
+
+				if(oldRoute && oldRoute.request)
 				{
-					//desc += "\n### Parameters"
-					var oldFormdata = null
-
-					if(oldRouteIndex && collection.item[oldRouteIndex].request)
+					if(route.enctype == 'form-data')
 					{
-						if(route.enctype == 'form-data')
-						{
-							oldFormdata = collection.item[oldRouteIndex].request.body.formdata
-						}
-
-						if(route.enctype == 'application/x-www-form-urlencoded')
-						{
-							oldFormdata = collection.item[oldRouteIndex].request.body.urlencoded
-						}
+						oldFormdata = oldRoute.request.body.formdata
 					}
 
-					for(var key in route.params)
+					if(route.enctype == 'application/x-www-form-urlencoded')
 					{
-						const param = route.params[key]
-
-						param.description = param.description || ''
-
-						//inital field value
-						var oldField = null
-
-						//search for the original value if there is one
-						if(oldRouteIndex)
-						{
-							for(var f in oldFormdata)
-							{
-								if(oldFormdata[f].key == key)
-								{
-									oldField = oldFormdata[f]
-								}
-							}
-						}
-
-						const exampleVal = typeof(param.example) != 'undefined' ? ( (param.schema.type) == 'string'? param.example : JSON.stringify(param.example)) : null 
-
-						formdata.push(
-						{
-							key: key,
-							type: 'text',
-							value: exampleVal ? exampleVal : ( oldField ? oldField.value : '' ),
-							description: `(${param.schema.type}, ${param.required ? 'required' : 'optional'}) ${param.description}`
-						})
-
-						/*
-						desc += "\n"
-						desc += `**${param.type}** ${key} *${param.required ? "required" : "optional"}* ${param.description}`
-						desc += "\n\n    "
-						desc += JSON.stringify(param, null, 4) + "\n"
-						*/
+						oldFormdata = oldRoute.request.body.urlencoded
 					}
 				}
 
+				//join bodyparams and files into one object
+				//both should be defined at this point thanks to add()
+				//they should not overlap
+				const params = Object.assign({}, route.params, route.files)
+
+				//move the parameters into the formdata
+				paramsToFormdata(params, formdata, oldFormdata)
+
+				//move the query parameters into the url query//move the parameters into the formdata
+				const query = []
+
+				paramsToFormdata(route.query, query, oldRoute ? oldRoute.request.url.query : [])
+				
+
+				//the new route to add to collection.item
 				const newRoute = 
 				{
 					name: route.name,
@@ -534,61 +594,74 @@ class PostmanRouter
 					},
 					request:
 					{
-						url: this.protocol + '://' + path.join(this.host, this.mountpath, route.route),
+						url: 
+						{
+							protocol: this.protocol,
+							host: this.host,
+							port: this.port,
+							path: path.join(this.mountpath, route.route),
+							query: query
+						},
 						description: desc,
 						method: route.method,
+						//use formdata by default
 						body:
 						{
 							mode: 'formdata',
 							formdata: formdata
-						}
+						},
+						//initialize an empty header in order to add values
+						header: []
 					}
 				}
 
-				if(this.enctype != 'form-data')
+				if(route.enctype != 'form-data')
 				{
-					newRoute.request.header = 
-					[
-						{
-							key: 'Content-Type',
-							name: 'Content-Type',
-							value: this.enctype,
-							type: 'text' 
-						}
-					]
+					newRoute.request.header.push( 
+					{
+						key: 'Content-Type',
+						name: 'Content-Type',
+						value: route.enctype,
+						type: 'text' 
+					})
 
-					if(this.enctype == 'application/x-www-form-urlencoded')
+					if(route.enctype == 'application/x-www-form-urlencoded')
 					{
 						newRoute.request.body.mode = 'urlencoded'
 						newRoute.request.body.urlencoded = newRoute.request.body.formdata
 						delete newRoute.request.body.formdata
 					}
+
 				}
 
-				if(oldRouteIndex)
+
+				if(oldRoute)
 				{
 					//keep some of the data from the current collection
 					//that way we don't change the order or any values, folders etc.
-					newRoute._postman_id = collection.item[oldRouteIndex]._postman_id
+					newRoute._postman_id = oldRoute._postman_id
 
-					newRoute.response = collection.item[oldRouteIndex].response
+					newRoute.response = oldRoute.response
 
 					//replace the route
 					collection.item[oldRouteIndex] = newRoute
 
 				}else
 				{
+					//add a new route
 					collection.item.push(newRoute)
+					console.log("Adding new endpoint " + route.route)
 				}
 			}
 		}
 	}
 
+	//TODO replace this with the static methods
 	updatePostman()
 	{
 		return new Promise((resolve, reject) =>
 		{
-			const apiKey = this.postman.apikey
+			const apikey = this.postman.apikey
 
 			const collection_uid = this.postman.collection_uid
 
@@ -600,7 +673,7 @@ class PostmanRouter
 				url: url,
 				headers:
 				{
-					'X-Api-Key': apiKey
+					'X-Api-Key': apikey
 				},
 				method: 'GET'
 			}, (err, res, body) =>
@@ -617,9 +690,12 @@ class PostmanRouter
 					//cheap way of saying that nothing has changed
 					if(newCollection == body)
 					{
+						console.log("Collection already up to date")
 						resolve()
 						return
 					}
+
+					console.log('Attempting to update collection via API')
 
 					//update the collection via the postman api
 					request(
@@ -627,7 +703,7 @@ class PostmanRouter
 						url: url,
 						headers:
 						{
-							'X-Api-Key': apiKey
+							'X-Api-Key': apikey
 						},
 						method: 'PUT',
 						body: newCollection
@@ -683,7 +759,7 @@ function getAllEndpoints(confignames)
 }
 module.exports.getAllEndpoints = getAllEndpoints
 
-function getPostmanCollection(apiKey, collection_iud)
+function getPostmanCollection(apikey, collection_uid)
 {
 	return new Promise((resolve, reject) =>
 	{
@@ -693,7 +769,7 @@ function getPostmanCollection(apiKey, collection_iud)
 				url: url,
 				headers:
 				{
-					'X-Api-Key': apiKey
+					'X-Api-Key': apikey
 				},
 				method: 'GET'
 				}, (err, res, body) =>
@@ -719,43 +795,84 @@ function getPostmanCollection(apiKey, collection_iud)
 		})
 }
 
-function updatePostmanCollection(apiKey, collection_iud, collection)
+function updatePostmanCollection(apikey, collection_uid, collection)
 {
 	return new Promise((resolve, reject) =>
 	{
-			const url = 'https://api.getpostman.com/collections/' + collection_uid
+		const url = 'https://api.getpostman.com/collections/' + collection_uid
+			
+		const body = JSON.stringify(collection)
 
-			const body = JSON.stringify(collection)
-
-			request(
+		request(
+		{
+			url: url,
+			headers:
 			{
-				url: url,
-				headers:
-				{
-					'X-Api-Key': apiKey
-				},
-				method: 'POST',
-				body: body,
+				'X-Api-Key': apikey
+			},
+			method: 'PUT',
+			body: body,
 
-				}, (err, res, body) =>
-				{
-				if(!err)
-				{
-					const response = JSON.parse(body)
+		}, (err, res, body) =>
+		{
+			if(!err)
+			{
+				const response = JSON.parse(body)
 
-					if(response.collection)
-					{
-						resolve(response)
-
-					}else
-					{
-						reject(response)
-					}
+				if(!response.error)
+				{
+					resolve(response)
 
 				}else
 				{
-					reject(err)
+					reject(response)
 				}
-			})
+
+			}else
+			{
+				reject(err)
+			}
 		})
+	})
+}
+
+
+//TODO: indentation
+function paramsToFormdata(params, formdata, oldFormdata)
+{
+	oldFormdata = oldFormdata || []
+
+	for(var pname in params)
+	{
+		const param = params[pname]
+
+		param.description = param.description || ''
+
+						//inital field object
+					var oldField = null
+
+					//search for the original value if there is one
+					for(var fieldName in oldFormdata)
+					{
+						if(oldFormdata[fieldName].key == pname)
+						{
+							oldField = oldFormdata[fieldName]
+						}
+					}
+						
+					//set the example val if the param has one
+					//if it's a string, there is no need to stringify it
+					const exampleVal = typeof(param.example) != 'undefined' ? ( (param.schema.type) == 'string'? param.example : JSON.stringify(param.example)) : null 
+
+					//add the field to the formdata
+					formdata.push(
+					{
+						key: pname,
+						type: param.type || 'text',
+						//exampleVal will take priority over values set in the postman app
+						value: exampleVal ? exampleVal : ( oldField ? oldField.value : param.type == 'file' ? null : '' ),
+						//description containing the type, required and the parameter description
+						description: `(${param.type || param.schema.type}, ${param.required ? 'required' : 'optional'}) ${param.description}`
+					})
+				}
 }
